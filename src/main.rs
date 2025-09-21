352033020f2bfa42397170f00df99d3204e5d3a8
Heminturen <heminturen@gmail.com>
2025-09-21 12:17:30 +0200

    It works with some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,126 @@
 use eframe::egui;
-use egui::{Button, CentralPanel, TopBottomPanel, Vec2};
+use eframe::egui::{CollapsingHeader, Id, Ui};
 
-struct Editor {
-    items: Vec<String>,
-    from: Option<usize>,
-    to: Option<usize>,
+use egui_dnd::{DragDropItem, Handle, dnd};
+
+pub fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        ..Default::default()
+    };
+    eframe::run_native("DnD", options, Box::new(|_cc| Ok(Box::<MyApp>::default())))
 }
 
-impl Default for Editor {
+#[derive(Default)]
+struct Tag {
+    name: String,
+
+    children: Option<Vec<Tag>>,
+}
+
+impl DragDropItem for &mut Tag {
+    fn id(&self) -> Id {
+        Id::new(&self.name)
+    }
+}
+
+struct MyApp {
+    items: Vec<Tag>,
+}
+
+impl Default for MyApp {
     fn default() -> Self {
-        Self {
-            items: vec!["A", "B", "C", "D", "E", "F"]
-                .into_iter()
-                .map(|i| i.to_string())
-                .collect(),
-            from: None,
-            to: None,
+        MyApp {
+            items: vec![
+                Tag {
+                    name: "Material".to_string(),
+                    ..Tag::default()
+                },
+                Tag {
+                    name: "Food".to_string(),
+                    ..Tag::default()
+                },
+                Tag {
+                    name: "Weapon".to_string(),
+                    ..Tag::default()
+                },
+                Tag {
+                    name: "Armor".to_string(),
+                    children: Some(vec![
+                        Tag {
+                            name: "Helmet".to_string(),
+                            ..Tag::default()
+                        },
+                        Tag {
+                            name: "Chest".to_string(),
+                            ..Tag::default()
+                        },
+                        Tag {
+                            name: "Leggins".to_string(),
+                            ..Tag::default()
+                        },
+                        Tag {
+                            name: "Boots".to_string(),
+                            ..Tag::default()
+                        },
+                    ]),
+                },
+            ],
         }
     }
 }
 
-impl eframe::App for Editor {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        TopBottomPanel::top("Header").show(ctx, |ui| {
-            ui.centered_and_justified(|ui| {
-                ui.label("This is imple example of drag and drop in egui.");
-            });
-        });
+impl MyApp {
+    fn draw_item(ui: &mut Ui, item: &mut Tag, handle: Handle) {
+        handle.ui(ui, |ui| {
+            if let Some(children) = &mut item.children {
+                CollapsingHeader::new(&item.name)
+                    .default_open(true)
+                    .show(ui, |ui| {
+                        let response = dnd(ui, "dnd_shared_id").show(
+                            children.iter_mut(),
+                            |ui, item, handle, _pressed| {
+                                Self::draw_item(ui, item, handle);
+                            },
+                        );
 
-        CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal_centered(|ui| {
-                for (id, item) in self.items.iter().enumerate() {
-                    let item_id = egui::Id::new((id, item.clone()));
-                    let response = ui
-                        .dnd_drag_source(item_id, id, |ui| {
-                            ui.add_sized(Vec2::splat(64.0), Button::selectable(false, item));
-                        })
-                        .response;
+                        response.update_vec(children);
+                    });
+            } else {
+                let response = ui.selectable_label(false, &item.name);
 
-                    if let (Some(pointer), Some(hovered_payload)) = (
-                        ui.input(|i| i.pointer.interact_pos()),
-                        response.dnd_hover_payload::<usize>(),
-                    ) {
-                        // INFO: Here implement visual where payload put
-                        let rect = response.rect;
-                    }
-
-                    if let Some(dragged_payload) = response.dnd_release_payload::<usize>() {
-                        self.from = Some(*dragged_payload);
-                        self.to = Some(id);
-                    }
-
-                    if response.hovered() {
-                        response.highlight();
-                    }
+                if response.hovered() {
+                    response.highlight();
                 }
-            });
+            }
         });
+
+        // if let Some(children) = &mut item.children {
+        //     CollapsingHeader::new("children")
+        //         .default_open(true)
+        //         .show(ui, |ui| {
+        //             ui.label("Content");
+        //
+        //             let response = dnd(ui, &item.name).show(
+        //                 children.iter_mut(),
+        //                 |ui, item, handle, _pressed| {
+        //                     Self::draw_item(ui, item, handle);
+        //                 },
+        //             );
+        //
+        //             response.update_vec(children);
+        //         });
+        // }
     }
 }
 
-fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Editor",
-        options,
-        Box::new(|_cc| Ok(Box::new(Editor::default()))),
-    )
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let response =
+                dnd(ui, "dnd_example").show(self.items.iter_mut(), |ui, item, handle, _pressed| {
+                    MyApp::draw_item(ui, item, handle);
+                });
+            response.update_vec(&mut self.items);
+        });
+    }
 }
