7578813b4feb9a581eac7c406ba91f63fecb1eda
Heminturen <heminturen@gmail.com>
2025-09-27 10:17:46 +0200

    Rework editor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use eframe::egui;
-use eframe::egui::{CollapsingHeader, Id, Ui};
+use eframe::egui::{Id, Ui};
 
+use egui::{Direction, Layout, ScrollArea, SidePanel, TextEdit, TopBottomPanel};
 use egui_dnd::{DragDropItem, Handle, dnd};
 
 pub fn main() -> eframe::Result<()> {
@@ -11,116 +12,110 @@ pub fn main() -> eframe::Result<()> {
 }
 
 #[derive(Default)]
-struct Tag {
+struct Item {
+    id: u16,
     name: String,
-
-    children: Option<Vec<Tag>>,
+    description: String,
 }
 
-impl DragDropItem for &mut Tag {
-    fn id(&self) -> Id {
-        Id::new(&self.name)
-    }
-}
-
-struct MyApp {
-    items: Vec<Tag>,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        MyApp {
-            items: vec![
-                Tag {
-                    name: "Material".to_string(),
-                    ..Tag::default()
-                },
-                Tag {
-                    name: "Food".to_string(),
-                    ..Tag::default()
-                },
-                Tag {
-                    name: "Weapon".to_string(),
-                    ..Tag::default()
-                },
-                Tag {
-                    name: "Armor".to_string(),
-                    children: Some(vec![
-                        Tag {
-                            name: "Helmet".to_string(),
-                            ..Tag::default()
-                        },
-                        Tag {
-                            name: "Chest".to_string(),
-                            ..Tag::default()
-                        },
-                        Tag {
-                            name: "Leggins".to_string(),
-                            ..Tag::default()
-                        },
-                        Tag {
-                            name: "Boots".to_string(),
-                            ..Tag::default()
-                        },
-                    ]),
-                },
-            ],
+impl Item {
+    fn new(id: u16) -> Item {
+        Item {
+            id,
+            name: String::from("Item"),
+            description: String::from("Brilliant description"),
         }
     }
 }
 
+impl DragDropItem for &mut Item {
+    fn id(&self) -> Id {
+        Id::new(self.id)
+    }
+}
+
+#[derive(Default)]
+struct MyApp {
+    items: Vec<Item>,
+    available_id: u16,
+}
+
 impl MyApp {
-    fn draw_item(ui: &mut Ui, item: &mut Tag, handle: Handle) {
+    fn draw_item_label(ui: &mut Ui, item: &mut Item, handle: Handle) {
         handle.ui(ui, |ui| {
-            if let Some(children) = &mut item.children {
-                CollapsingHeader::new(&item.name)
-                    .default_open(true)
-                    .show(ui, |ui| {
-                        let response = dnd(ui, "dnd_shared_id").show(
-                            children.iter_mut(),
-                            |ui, item, handle, _pressed| {
-                                Self::draw_item(ui, item, handle);
-                            },
-                        );
+            let response = ui.selectable_label(false, &item.name);
 
-                        response.update_vec(children);
-                    });
-            } else {
-                let response = ui.selectable_label(false, &item.name);
-
-                if response.hovered() {
-                    response.highlight();
-                }
+            if response.hovered() {
+                response.highlight();
             }
         });
+    }
 
-        // if let Some(children) = &mut item.children {
-        //     CollapsingHeader::new("children")
-        //         .default_open(true)
-        //         .show(ui, |ui| {
-        //             ui.label("Content");
-        //
-        //             let response = dnd(ui, &item.name).show(
-        //                 children.iter_mut(),
-        //                 |ui, item, handle, _pressed| {
-        //                     Self::draw_item(ui, item, handle);
-        //                 },
-        //             );
-        //
-        //             response.update_vec(children);
-        //         });
-        // }
+    fn add_item(&mut self) {
+        self.items.push(Item::new(self.available_id));
+        self.available_id += 1;
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let response =
-                dnd(ui, "dnd_example").show(self.items.iter_mut(), |ui, item, handle, _pressed| {
-                    MyApp::draw_item(ui, item, handle);
+        TopBottomPanel::new(egui::panel::TopBottomSide::Top, "Header").show(ctx, |ui| {
+            ui.with_layout(
+                Layout::centered_and_justified(Direction::LeftToRight),
+                |ui| {
+                    ui.label(egui::RichText::new("Item editor").heading());
+                },
+            )
+        });
+
+        SidePanel::left("Item view")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.heading("Items");
+                    ui.separator();
                 });
-            response.update_vec(&mut self.items);
+
+                let mut input = String::new();
+                let text_edit = TextEdit::singleline(&mut input).hint_text("Search your items");
+                ui.add(text_edit);
+
+                ScrollArea::vertical().show(ui, |ui| {
+                    ui.set_width(ui.available_width());
+                    ui.set_height(ui.available_height() - 32.0);
+                    let response = dnd(ui, "root").show(
+                        self.items.iter_mut(),
+                        |ui, item, handle, _pressed| {
+                            MyApp::draw_item_label(ui, item, handle);
+                        },
+                    );
+                    response.update_vec(&mut self.items);
+                });
+
+                ui.separator();
+
+                ui.with_layout(
+                    Layout::centered_and_justified(Direction::LeftToRight),
+                    |ui| {
+                        let add_item_button = ui.button("Add new item");
+
+                        if add_item_button.clicked() {
+                            self.add_item();
+                            // self.items.push(Item::default());
+                        }
+                    },
+                );
+
+                Layout::horizontal_align
+            });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Here edit your item");
+            // let response =
+            //     dnd(ui, "root").show(self.items.iter_mut(), |ui, item, handle, _pressed| {
+            //         MyApp::draw_item(ui, item, handle);
+            //     });
+            // response.update_vec(&mut self.items);
         });
     }
 }
